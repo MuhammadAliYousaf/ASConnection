ff0fd87b4ae2de89af5aa2bb1bdb7cc9fac3cbc1
alisolutions <m.aliyousaf786@gmail.com>
2015-03-03 15:34:11 +0500

    Sample post request is added

--- ASConnectionTest/ASConnectionTest/ViewController.m
+++ ASConnectionTest/ASConnectionTest/ViewController.m
@@ -19,8 +19,22 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    /* Sample GET Request with Delegate */
     ASConnection *connection = [[ASConnection alloc] initWithUrl:@"https://api.app.net/stream/0/posts/stream/global" delegate:self];
     [connection invokeMethod:kHttpMethod_Get query:nil requestData:nil contentType:nil];
+    
+    
+    
+    
+    
+    
+    
+    
+//    /* Sample POST Request with Delegate */
+//    ASConnection *connection = [[ASConnection alloc] initWithUrl:@"https://importexport.amazonaws.com" delegate:self];
+//    NSString *postDataBody = @"Action=GetStatus&SignatureMethod=HmacSHA256&JobId=JOBID&SignatureVersion=2&Version=2014-12-18&Signature=%2FVfkltRBOoSUi1sWxRzN8rw%3D&Timestamp=2014-12-20T22%3A30%3A59.556Z";
+//    NSData *data = [postDataBody dataUsingEncoding:NSUTF8StringEncoding];
+//    [connection invokeMethod:kHttpMethod_Post query:nil requestData:data contentType:@"application/x-www-form-urlencoded;charset=utf-8"];
     
 }
 
